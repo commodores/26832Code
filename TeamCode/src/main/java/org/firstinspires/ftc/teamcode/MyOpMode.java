a96c30f880ec5fe0536253cdedc3669d1ab7b5ae
FTC Laptop <dphagler@gmail.com>
2024-10-23 15:24:54 -0400

    works!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyOpMode.java
@@ -1,23 +1,30 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
 @TeleOp
 
 public class MyOpMode extends LinearOpMode {
     private Servo clawServo;
     private DcMotorSimple armMotor = null;
+    private DcMotorSimple elevatorMotor = null;
 
 
 
     @Override
     public void runOpMode() {
+
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
         clawServo = hardwareMap.get(Servo.class, "clawServo");
         armMotor  = hardwareMap.get(DcMotorSimple.class, "armMotor");
+        elevatorMotor  = hardwareMap.get(DcMotorSimple.class, "elevatorMotor");
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -28,6 +35,7 @@ public class MyOpMode extends LinearOpMode {
         while (opModeIsActive()) {
 
             double armPower = .5;
+            double elevatorPower = .75;
 
             // check to see if we need to move the servo.
 
@@ -41,14 +49,35 @@ public class MyOpMode extends LinearOpMode {
 
             // check to see if we move the arm.
             if(gamepad1.x) {
-                // move forward.
-                armMotor.setPower(armPower);
+                // move up.
+                armMotor.setPower(-armPower);
             } else if (gamepad1.a) {
-                // move reverse.
+                // move down.
                 armMotor.setPower(armPower);
             } else {
                 armMotor.setPower(0);
             }
+
+            // check to see if we move the elevator.
+            if(gamepad1.left_bumper) {
+                // move down.
+                elevatorMotor.setPower(-elevatorPower);
+            } else if (gamepad1.right_bumper) {
+                // move up.
+                elevatorMotor.setPower(elevatorPower);
+            } else {
+                elevatorMotor.setPower(0);
+            }
+
+            //run drivetrain
+            drive.setWeightedDrivePower(
+                    new Pose2d(
+                            -gamepad1.left_stick_y,
+                            -gamepad1.left_stick_x,
+                            -gamepad1.right_stick_x
+                    )
+            );
+            drive.update();
 
             telemetry.addData("Servo Position", clawServo.getPosition());
 
